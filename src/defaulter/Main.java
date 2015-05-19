937a4365019b193b4b770f86bbe695418d138fab
Ezequiel Tomas Lynch <elynch@lab64.it.itba.edu.ar>
2015-05-19 19:02:34 -0300

    primera version swingX

--- src/defaulter/Main.java
+++ src/defaulter/Main.java
@@ -1,5 +1,8 @@
 package defaulter;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class Main {
 
 	public static void main(String[] args/*agregar q se pase por parametro*/) {
@@ -11,6 +14,29 @@ public class Main {
 		} else {
 			level.print();
 		}
+		pres(level);
 	}
+		
+	public static void pres(Level level){
+		final Level level2 = level;
+		Runnable r = new Runnable() {
+			@Override
+            public void run() {
+                LevelDrawer cb =
+                        new LevelDrawer(level2);
 
+                JFrame f = new JFrame("level");
+                f.add(cb.getGui());
+                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                f.setLocationByPlatform(true);
+
+                f.pack();
+                f.setMinimumSize(f.getSize());
+                f.setVisible(true);
+            }
+        };
+        SwingUtilities.invokeLater(r);
+	}
 }
+
+
